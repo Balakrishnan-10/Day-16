08b037718a79fc3ee6affb844cea343d96d02156
Balakrishnan-10 <krishnabala1495@gmail.com>
2024-02-27 06:18:25 +0530

    Create Callback Hell File

---
+++ script.js
@@ -0,0 +1,37 @@
+var div = document.createElement("div");
+document.body.append(div);
+
+setTimeout (function(){
+document.querySelector("div").innerText = `Counter:${10}`;
+setTimeout (function(){
+    document.querySelector("div").innerText = `Counter:${9}`;
+    setTimeout (function(){
+    document.querySelector("div").innerText = `Counter:${8}`;
+    setTimeout (function(){
+    document.querySelector("div").innerText = `Counter:${7}`;
+    setTimeout (function(){
+    document.querySelector("div").innerText = `Counter:${6}`;
+    setTimeout (function(){
+    document.querySelector("div").innerText = `Counter:${5}`;
+    setTimeout (function(){
+    document.querySelector("div").innerText = `Counter:${4}`;
+    setTimeout (function(){
+    document.querySelector("div").innerText = `Counter:${3}`;
+    setTimeout (function(){
+    document.querySelector("div").innerText = `Counter:${2}`;
+    setTimeout (function(){
+    document.querySelector("div").innerText = `Counter:${1}`;
+    setTimeout (function(){
+    document.querySelector("div").innerText = `Happy Independance Day`
+    
+                  },1000)
+                 },1000)
+                },1000)
+               },1000)
+              },1000)
+             },1000)
+            },1000)
+        },1000)
+       },1000)
+      },1000)
+    },1000)
